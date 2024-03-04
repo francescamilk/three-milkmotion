6058adcf7166dd4b513071bf03e101facf5a6c9b
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-04 18:03:00 +0100

    tweak style & add basic meshes

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ const gui    = new GUI();
 const params = {
     materialColor: '#ffeded'
 };
-
 gui.addColor(params, 'materialColor');
+
+// toggle gui controls
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'h')
+        gui.show(gui._hidden);
+});
 
 // Canvas
 const canvas = document.querySelector('canvas#webgl');
@@ -15,12 +20,30 @@ const canvas = document.querySelector('canvas#webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// REF(test cube)
+// Objects    REF(test cube)
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 );
 scene.add(cube);
+
+// Meshes
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+scene.add(mesh1, mesh2, mesh3);
 
 // Window
 const sizes = {
@@ -28,8 +51,7 @@ const sizes = {
     height: window.innerHeight
 };
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // update sizes
     sizes.width  = window.innerWidth;
     sizes.height = window.innerHeight;
@@ -41,7 +63,7 @@ window.addEventListener('resize', () =>
     // update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-})
+});
 
 // Camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100);
@@ -49,14 +71,18 @@ camera.position.z = 6;
 scene.add(camera);
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ 
+    canvas,
+    alpha: true
+});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// renderer.setClearAlpha(0);
+
 // Animate
 // const clock    = new THREE.Clock();
-const animLoop = () =>
-{
+const animLoop = () => {
     // const elapsedTime = clock.getElapsedTime();
     // call again on the next frame
     renderer.render(scene, camera);
